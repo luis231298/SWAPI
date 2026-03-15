2d8cc0657ca393d1b87c0f19dd092185360c231c
luis231298 <pepeluna2385@gmail.com>
2026-03-14 18:59:16 -0600

    Update extract.py
    Se prueba una alternativa con recursividad. Pero no se guarda el ultimo

--- extract.py
+++ extract.py
@@ -1,7 +1,32 @@
 import requests
 
-url = "https://swapi.dev/api/people/"
-response = requests.get(url)
-data = response.json()
+def getData(url, all_data):
+    try:
+        response = requests.get(url)
+        print("Status Code: ", response.status_code)
+        if response.status_code == 200:
+            data = response.json()
+            if data["next"]:
+                all_data.append(data)
+                getData(data["next"], all_data)  
+            return all_data
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Status Code: ", response.status_code)
+        print(f"Error al obtener la data: {e}")
+        return None
 
-print(data)
+def main():
+    all_data = []
+    #Obtención de la data de la API. Rubro: People
+    data = getData("https://swapi.dev/api/people/", all_data)
+
+
+    if data:
+        print(data)
+    else:
+        print("Error al obtener la data")
+
+if __name__ == "__main__":
+    main()
