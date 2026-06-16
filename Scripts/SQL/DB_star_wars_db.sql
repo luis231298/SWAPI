216f87ff17d7365fd2ac5848b2df383d59631648
luis231298 <pepeluna23@yahoo.com>
2026-06-15 18:52:43 -0600

    Update DB_star_wars_db.sql
    Se agrega la sección para la creación de usuarios y permisos

--- Scripts/SQL/DB_star_wars_db.sql
+++ Scripts/SQL/DB_star_wars_db.sql
@@ -1,8 +1,9 @@
-
+--Creación de Base de datos y esquema
 CREATE DATABASE IF NOT EXISTS star_wars_db;
 
 CREATE SCHEMA IF NOT EXISTS silver;
 
+--Creación de Tablas separadas por DIM y FACTS.
 CREATE TABLE silver.dim_planets (
 id INTEGER PRIMARY KEY,
 name VARCHAR(100),
@@ -38,3 +39,32 @@ CREATE TABLE silver.fact_planet_residents(
 planet_id INTEGER REFERENCES silver.dim_planets(id),
 resident_id INTEGER REFERENCES silver.dim_people(id)
 );
+
+--Creación de usuarios y dandole los permisos necesarios
+CREATE USER etl_user WITH PASSWORD 'etl123';
+GRANT CONNECT ON DATABASE star_wars_db TO etl_user;
+GRANT USAGE,CREATE ON SCHEMA silver TO etl_user;
+
+-- Permisos para las tablas que YA existen hoy
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA silver TO etl_user;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA silver TO etl_user;
+
+--Tablas futuras
+ALTER DEFAULT PRIVILEGES IN SCHEMA silver 
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO etl_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA silver 
+GRANT USAGE, SELECT ON SEQUENCES TO etl_user;
+
+CREATE USER dashboard_user WITH PASSWORD 'dash123';
+GRANT CONNECT ON DATABASE star_wars_db TO dashboard_user;
+GRANT USAGE ON SCHEMA silver TO dashboard_user;
+
+-- Permisos para las tablas que YA existen hoy 
+GRANT SELECT ON ALL TABLES IN SCHEMA silver TO dashboard_user;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA silver TO dashboard_user;
+
+--Tablas futuras
+ALTER DEFAULT PRIVILEGES IN SCHEMA silver 
+GRANT SELECT ON TABLES TO dashboard_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA silver 
+GRANT SELECT ON SEQUENCES TO dashboard_user;
